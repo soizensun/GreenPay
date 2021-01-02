3154ecdf987e4c70bdf2db6cf2c2ab0b9720f622
kitpavin <kitpavin@gmail.com>
2021-01-02 11:35:05 +0700

    create navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,19 @@
-import '../styles/globals.css'
+
 import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <div style={{ fontFamily: 'Mitr' }}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@300&display=swap" rel="stylesheet"></link>
       </Head>
-      < Component {...pageProps } />
-    </>
-  ) 
+      < Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,24 @@
-import { useEffect } from 'react'
 import MainLayout from "../layouts/MainLayout";
-import { GoogleLogin } from 'react-google-login';
 import Axios from 'axios'
+import CustomButton from '../components/CustomButton'
 
-
-const HEADERS = { 'Content-Type': 'application/json' }
 export default function Home() {
 
-  const responseGoogle = (response) => {
-    console.log(response);
+  return (
+    <div>
+      <MainLayout>
 
-    Axios.post('/api/googleAuth', JSON.stringify({ "tokenId": response.tokenId }), { headers: HEADERS })
-    .then(res => {
-        console.log(res.data);
-    })
-    .catch(err => {
-    })
-  }
+        <main>
+          <div style={{ height: "4000px", marginTop: "100px"}}>
+            <CustomButton/>
+            asdfasdfbbbb
+          </div>
 
-    return (
-      <div>
-        <MainLayout>
-
-          <main>
-            <GoogleLogin
-              clientId="1068628232562-qm0ssc22ls4ks62jcopg1frqbdau8jau.apps.googleusercontent.com"
-              buttonText="Login with google"
-              onSuccess={responseGoogle}
-              onFailure={responseGoogle}
-              cookiePolicy={'single_host_origin'}
-            />
-          </main>
-        </MainLayout>
-      </div>
-    )
-  }
+        </main>
+      </MainLayout>
+    </div>
+  )
+}
 
 
 
