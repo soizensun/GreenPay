8f012dd664c7b74dce8beb6ce70ac3693561e8a9
kitpavin <kitpavin@gmail.com>
2021-01-05 00:18:14 +0700

    after delete stash

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,8 +19,6 @@ export default function Home() {
             <ProductCard/>
             <ProductCard/>
             <ProductCard/>
-            <CustomButton/>
-            asdfasdfbbbb
           </div>
 
         </main>
